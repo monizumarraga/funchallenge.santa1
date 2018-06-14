a6ecf202ba9ebf595ce9de5c0e062ffb47184c2b
monizumarraga <39333481+monizumarraga@users.noreply.github.com>
2018-06-14 19:59:11 +0200

    question 2

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ fs.readFile('./santa.txt',(err,data) =>{
 	}
 	const route=data.toString().split('');
 	let floor= (route.filter(top=>{return top==="("})).length-(route.filter(top=>{return top===")"})).length
-	console.log(floor);
+	console.log("last floor is "+floor);
 	console.timeEnd('funchallenge');
+
+	floor=0;
+	let floor_down=0
+	route.forEach((dir, num) => {
+		if (dir==="("){
+			floor=floor+1;
+		}else{
+			floor=floor-1;
+		}
+		if ((floor_down===0)&&(floor<0)){
+			floor_down=num+1;
+		}
+	})
+	console.log("first instruction in minus floor is "+floor_down);
 })
